6fbb5f62b64c9dbce61c0b7409175ce47c2cd43c
HaojieZhang123 <haojiezhang123@gmail.com>
2025-04-14 17:25:24 +0200

    [UPD] created function that return object with full name

---
+++ modules/names.js
@@ -0,0 +1,9 @@
+function fullName(firstNameVar, lastNameVar) {
+    // create object with first and last name
+    const person = {
+        firstName: firstNameVar,
+        lastName: lastNameVar
+    };
+    return person;
+}
+
