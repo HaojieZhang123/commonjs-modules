5b9c17d441f24974ab08f9c76d17761bdc899bd8
HaojieZhang123 <haojiezhang123@gmail.com>
2025-04-14 17:41:34 +0200

    [UPD[ created function for people, using the imported functions

--- people.js
+++ people.js
@@ -1,3 +1,14 @@
 // import fullName and hobbies from names.js and hobbies.js
 const fullName = require('./modules/names.js');
 const hobbies = require('./modules/hobbies.js');
+
+// function with no parameters that returns an object with fullName and hobbies properties
+function people() {
+    return {
+        fullName: fullName('John', 'Doe'),
+        hobbies: hobbies('Coding', 'Reading', 'Swimming')
+    }
+}
+
+// calling function and logging the result
+console.log(people());
